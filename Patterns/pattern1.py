5d2334503353270814fac06ae04e4a3c5786eb0c
akshay <sakshaynarayan@gmail.com>
2026-08-12 21:51:16 +0530

    Add pattern 1 solution

---
+++ Patterns/pattern1.py
@@ -0,0 +1,11 @@
+class solution:
+    def pattern(self,N):
+        for i in range(N):
+            for j in range(N):
+                print("* ", end="")
+            print()
+
+if __name__ == "__main__":
+    sol = solution()
+    N=5
+    sol.pattern(N)
